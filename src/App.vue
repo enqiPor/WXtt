d2d984dbf666e00b3e1f4ea733b8da30da57ea38
ZhangQinBao <zqb8686@163.com>
2020-09-03 17:07:20 +0800

    zhangqinbao

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
-
+<script>
+	export default{
+		created() {
+		  this.$axios.get('http://weixin.ingclass.org/webstage/weixin/common/login/getShareInfo.do?platCode=gh&current_url='+encodeURIComponent(window.location.href)).then(({data}) => {   
+			let weiXinDataObj={
+	    		  "appId":data.appId,
+	    		  "timestamp":data.timestamp,
+	    		  "nonceStr":data.nonceStr,
+	    		  "signature":data.signature,
+	    		  "link":data.link
+	      	} 
+			window.localStorage.setItem("weiXinDataObj",JSON.stringify(weiXinDataObj));
+		  })
+		}
+	}
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+	#app{
+		height: 100%;
+		width: 100%;
+	}
+	/*base*/	*{margin:0;padding:0;box-sizing: border-box;}	*:after, *::before {	  -webkit-box-sizing: border-box;	  -moz-box-sizing: border-box;	  box-sizing: border-box;	}	textarea{	  erflow: hidden;	  resize: none;	  padding: 10px;	}	i{	  font-style: inherit;	}	input,fieldset,img {	  border:0;	}	input,select,textarea{	  box-sizing: border-box;padding-left: 10px;	}	address,caption,cite,code,dfn,strong,th,var,optgroup {	  font-style:inherit;	  font-weight:inherit;	}	caption,th {	  text-align:left;	}	input,button,textarea,select,optgroup,option {	  font-family:inherit;	  font-size:inherit;	  font-style:inherit;	  font-weight:inherit;	  outline: none;	}	input{	  -webkit-appearance:none;	}	input::-webkit-search-cancel-button {	  display: none;	}	li{	  list-style:none;	}	/*i{font-style:normal}*/	h1,h2,h3,h4,h5,h6{	  font-size:12px;	  font-weight:400;	}	.clear:before,.clear:after{	  content:"";	  display:table;	  clear:both;	}	.fl{	  float:left;	}	.fr{	  float:right;	}	a{	  text-decoration:none;	}	a:link{	  color:#333;	}	a:hover{	  color:#333;	}	a:visited{	  color:#333;	}	input::-webkit-outer-spin-button,	input::-webkit-inner-spin-button{	  -webkit-appearance: none !important;	  margin: 0;	}	input[type="number"] {	  -moz-appearance: textfield;	}	em{	  font-style: inherit;	}	.flexs{	  display: flex;	  -webkit-display: flex;	  -moz-display: flex;	  -o-display: flex;	  align-items: center;	  -webkit-align-items: center;	  -moz-align-items: center;	  -o-align-items: center;	  justify-content: center;	  -webkit-justify-content: center;	  -moz-justify-content: center;	  -o-justify-content: center;	}	input[type="number"]{	  -moz-appearance:textfield;	}	body{	  font-family: "PingFang SC", "Hiragino Sans GB", "Hiragino Sans GB W3", "Microsoft Yahei", 微软雅黑, SimHei, 黑体, sans-serif;	  color: #333;	}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,177 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <transition name="scaleUp">
+    <div class="page" id="start">
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+     <p>上滑切换页面</p>
+    </div>
+  </transition>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+      return {
+        startX: 0, // 鼠标开始点击的x坐标
+        startY: 0 ,
+		}
+  },
+  methods: {
+    addHandler (element, type, handler) {
+        if (element.addEventListener) {
+          element.addEventListener(type, handler, false)
+        } else if (element.attachEvent) {
+          element.attachEvent('on' + type, handler)
+        } else {
+          element['on' + type] = handler
+        }
+      },
+    handleTouchEvent (event) {
+        switch (event.type) {
+          case 'touchstart':
+            this.startX = event.touches[0].pageX
+            this.startY = event.touches[0].pageY
+            break
+          case 'touchend':
+            var spanX = event.changedTouches[0].pageX - this.startX
+            var spanY = event.changedTouches[0].pageY - this.startY
+            // console.log('spanY', spanY)
+            if (spanY < -30) { // 向上
+				this.$router.replace('/userlist')
+            }
+            break
+        }
+	}
+  },
+  mounted () {
+	//禁止页面下拉
+	document.body.addEventListener('touchmove', function (e) {
+       e.preventDefault()
+    }, { passive: false })
+	
+	//上滑跳转
+	let element = document.getElementById('start')
+      this.addHandler(element, 'touchstart', this.handleTouchEvent)
+      this.addHandler(element, 'touchend', this.handleTouchEvent)
+      this.addHandler(element, 'touchmove', this.handleTouchEvent)
+	  
+	//微信分享
+	let imgUrl="";
+	let weiXinDataObj=JSON.parse(window.localStorage.getItem("weiXinDataObj"));
+	let shareTitle="宝宝 评估报告";//分享title内容
+	let shareCont="VB-MAPP综合评估及改善计划";//分享内容
+	let shareLink=weiXinDataObj.link;//分享链接
+	wx.config({
+		debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+		appId: weiXinDataObj.appId, // 必填，公众号的唯一标识
+		timestamp: weiXinDataObj.timestamp, // 必填，生成签名的时间戳
+		nonceStr: weiXinDataObj.nonceStr, // 必填，生成签名的随机串
+		signature: weiXinDataObj.signature,// 必填，签名，见附录1
+		jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage',"onMenuShareQQ","onMenuShareQZone"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+	});
+	wx.ready(function(){
+		//分享到朋友圈
+		wx.onMenuShareTimeline({
+			title: shareTitle, // 分享标题
+			link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			imgUrl: imgUrl, // 分享图标
+			success: function () {
+				// 用户确认分享后执行的回调函数
+			},
+			cancel: function () {
+				// 用户取消分享后执行的回调函数
+			}
+		});
+		//分享给朋友
+		wx.onMenuShareAppMessage({
+			title: shareTitle, // 分享标题
+			desc: shareCont, // 分享描述
+			link: shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+			imgUrl: imgUrl, // 分享图标
+			type: '', // 分享类型,music、video或link，不填默认为link
+			dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+			success: function () {
+				// 用户确认分享后执行的回调函数
+			},
+			cancel: function () {
+				// 用户取消分享后执行的回调函数
+			}
+		});
+		//分享到QQ
+		wx.onMenuShareQQ({
+			title: shareTitle, // 分享标题
+			desc: shareCont, // 分享描述
+			link: shareLink, // 分享链接
+			imgUrl: imgUrl, // 分享图标
+			success: function () {
+				// 用户确认分享后执行的回调函数
+			},
+			cancel: function () {
+				// 用户取消分享后执行的回调函数
+			}
+		});
+		//分享到空间
+		wx.onMenuShareQZone({
+			title: shareTitle, // 分享标题
+			desc: shareCont, // 分享描述
+			link: shareLink, // 分享链接
+			imgUrl: imgUrl, // 分享图标
+			success: function () {
+				// 用户确认分享后执行的回调函数
+			},
+			cancel: function () {
+				// 用户取消分享后执行的回调函数
+			}
+		});
+	});
   }
 }
 </script>
+<style lang="scss">
+	.scaleUp-enter-active, .scaleUp-leave-active {
+	  transition: all 1s linear;
+	 }
+	.scaleUp-leave-active {
+	  background: #000;
+	}
+	#start{
+		width: 100%;
+		height: 100%;
+	}
+	html,body{
+		height: 100%;
+		width: 100%;
+		overflow: hidden;
+	}
+</style>
