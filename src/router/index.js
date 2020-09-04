3ad579252e3d90971aa7dabbc5d4f2264edbe2a9
LingHuzh <7920916+linghuzh@user.noreply.gitee.com>
2020-09-04 17:01:54 +0800

    linghu

--- src/router/index.js
+++ src/router/index.js
@@ -20,14 +20,7 @@ Vue.use(VueRouter)
       name: 'ChildPage',
       component: () => import(/* webpackChunkName: "about" */ '../views/ChildPage.vue')
     },
-  {
-    path: '/video',
-    name: 'Video',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Video.vue')
-  }
+
 ]
 
 const router = new VueRouter({
--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -1,12 +1,41 @@
 <template>
   <transition name="scaleDown">
 	<div class="showPage" id="showPage">
+        <div class="airship"></div>
+<!--        包包-->
+        <div class="box-baobao-qiu">
+            <p class="header-baobao" @click="baobaoHeader"></p>
+        </div>
+<!--        豪豪-->
+        <div class="box-haohao-qiu">
+            <p class="header-haohao"  @click="haohaoHeader"></p>
+        </div>
+<!--        辰辰-->
+        <div class="box-chenchen-qiu">
+            <p class="header-chenchen" @click="chenchenHeader"></p>
+        </div>
+<!--        麦兜-->
+        <div class="box-maidou-qiu">
+            <p class="header-maidou" @click="maidouHeader"></p>
+        </div>
+<!--        江一-->
+        <div class="box-jiangyi-qiu">
+            <p class="header-jiangyi" @click="jiangyiHeader"></p>
+        </div>
+<!--        融融-->
+        <div class="box-rongrong-qiu">
+            <p class="header-rongrong" @click="rongrongHeader"></p>
+        </div>
+<!--        威利-->
+        <div class="box-weili-qiu">
+            <p class="header-weili" @click="weiliHeader"></p>
+        </div>
 <!--        七个孩子海报-->
-<!--        <div class="child-box chenchen">-->
-<!--            <img src="../../public/images/chenchen.jpg" alt="" class="chiild-image">-->
-<!--            <img src="../../public/images/guanbi.png" alt="" class="close-image">-->
-<!--            <img src="../../public/images/fenxiang.png" alt="" class="share-image">-->
-<!--        </div>-->
+        <div class="child-box chenchen" :status="childStatus">
+            <img :src=childImage alt="" class="chiild-image">
+            <img src="../../public/images/guanbi.png" alt="" class="close-image" @click="closeChild">
+            <img src="../../public/images/fenxiang.png" alt="" class="share-image">
+        </div>
 
 	</div>
   </transition>
@@ -19,9 +48,50 @@
 	      return {
 	          startX: 0, // 鼠标开始点击的x坐标
 	          startY: 0 ,
+              childStatus:'',
+              childImage:''
           }
 	  },
 	  methods: {
+          chenchenHeader(){
+              $('.child-box').fadeIn();
+              this.childStatus = 3;
+              this.childImage = require('../../public/images/chenchen.jpg');
+          },
+          baobaoHeader(){
+              $('.child-box').fadeIn();
+              this.childStatus = 1;
+              this.childImage = require('../../public/images/baobao.png');
+          },
+          haohaoHeader(){
+              $('.child-box').fadeIn();
+              this.childStatus = 2;
+              this.childImage = require('../../public/images/haohao.png');
+          },
+          maidouHeader(){
+              $('.child-box').fadeIn();
+              this.childStatus = 4;
+              this.childImage = require('../../public/images/maidou.png');
+          },
+          jiangyiHeader(){
+              $('.child-box').fadeIn();
+              this.childStatus = 5;
+              this.childImage = require('../../public/images/jiangyi.png');
+          },
+          rongrongHeader(){
+              $('.child-box').fadeIn();
+              this.childStatus = 6;
+              this.childImage = require('../../public/images/rongrogn.png');
+          },
+          weiliHeader(){
+              $('.child-box').fadeIn();
+              this.childStatus = 7;
+              this.childImage = require('../../public/images/weli.png');
+          },
+          closeChild(){
+              $(".child-box").fadeOut();
+              this.childImage = '';
+          },
 	    addHandler (element, type, handler) {
 	        if (element.addEventListener) {
 	          element.addEventListener(type, handler, false)
@@ -41,7 +111,6 @@
 	            var spanX = event.changedTouches[0].pageX - this.startX;
 	            var spanY = event.changedTouches[0].pageY - this.startY;
 	            // console.log('spanY', spanY)
-				console.log(spanY)
 	            // if (spanY > 30) { // 向上
 				// 	this.$router.replace('/')
 	            // }else if (spanY > -30){
@@ -149,7 +218,7 @@
 	#showPage{
 		width: 100%;
 		height: 100%;
-		background: url(../../public/img-bg/xingqiu_bg1.png) no-repeat;
+		background: url(../../public/img-bg/bg1.png) no-repeat;
 		background-size: 100% 100%;
 		-webkit-background-size: 100% 100%;
 		background-attachment: fixed;
@@ -171,6 +240,7 @@
         background-size: 100% 100%;
         box-sizing: border-box;
         position: relative;
+        display: none;
         img{
             display: block;
             position: absolute;
@@ -194,5 +264,146 @@
         bottom: 22px;
         left: 50%;
         margin-left: -54.5px;
+    }
+    /*飞船*/
+    .box-baobao-qiu,.airship{
+
+
+    }
+
+    .airship{
+        width: 154px;
+        height: 153px;
+        background: url("../../public/images/feichuan.png") no-repeat;
+        position: absolute;
+        background-size: 100% 100%;
+        top: 27px;
+        left: 11px;
+    }
+    .box-baobao-qiu{
+        width: 78px;
+        height: 51px;
+        position: absolute;
+        background: url("../../public/images/qiu1.png") no-repeat;
+        background-size: 100% 100%;
+        right: 17px;
+        top: 55px;
+    }
+    .header-baobao{
+        width: 42.5px;
+        height: 65.5px;
+        position: absolute;
+        background: url("../../public/images/bao.png") no-repeat;
+        background-size: 100% 100%;
+        top: -46px;
+        right: 59px;
+    }
+    .box-haohao-qiu{
+        width: 61px;
+        height: 77px;
+        position: absolute;
+        background: url("../../public/images/qiu2.png") no-repeat;
+        background-size: 100% 100%;
+        top: 203px;
+        left: 82px;
+    }
+    .header-haohao{
+        width: 43.5px;
+        height: 63px;
+        position: absolute;
+        background: url("../../public/images/hao.png") no-repeat;
+        background-size: 100% 100%;
+        top: -39px;
+        left: -21px;
+    }
+    .box-chenchen-qiu{
+        width: 89.5px;
+        height: 64px;
+        position: absolute;
+        background: url("../../public/images/qiu3.png") no-repeat;
+        background-size: 100% 100%;
+        top: 230px;
+        right: 21.5px;
+    }
+    .header-chenchen{
+        width: 42.5px;
+        height: 65.5px;
+        position: absolute;
+        background: url("../../public/images/chen.png") no-repeat;
+        background-size: 100% 100%;
+        top: -52px;
+        right: 65px;
+    }
+    .box-maidou-qiu{
+        width: 115px;
+        height: 80.5px;
+        position: absolute;
+        background: url("../../public/images/qiu4.png") no-repeat;
+        background-size: 100% 100%;
+        left: 0px;
+        top: 390px;
+    }
+    .header-maidou{
+        width: 43px;
+        height: 63px;
+        position: absolute;
+        background: url("../../public/images/mai.png") no-repeat;
+        background-size: 100% 100%;
+        top: -58px;
+        left: 64px;
+    }
+    .box-jiangyi-qiu{
+             width: 42px;
+             height: 42px;
+             position: absolute;
+             background: url("../../public/images/qiu5.png") no-repeat;
+             background-size: 100% 100%;
+             right: 23px;
+             top: 378.5px;
+         }
+    .header-jiangyi{
+        width: 43px;
+        height: 63px;
+        position: absolute;
+        background: url("../../public/images/jiang.png") no-repeat;
+        background-size: 100% 100%;
+        top: -50px;
+        right: 25px;
+    }
+    .box-rongrong-qiu{
+             width: 55.5px;
+             height: 53.5px;
+             position: absolute;
+             background: url("../../public/images/qiu6.png") no-repeat;
+             background-size: 100% 100%;
+             left: 110px;
+             top: 522px;
+         }
+    .header-rongrong{
+        width: 43px;
+        height: 63px;
+        position: absolute;
+        background: url("../../public/images/rong.png") no-repeat;
+        background-size: 100% 100%;
+        top: -56px;
+        right: -23px;
+    }
+    .box-weili-qiu{
+        width: 92px;
+        height: 68.5px;
+        position: absolute;
+        background: url("../../public/images/qiu7.png") no-repeat;
+        background-size: 100% 100%;
+        right: 51.5px;
+        bottom: 0;
+    }
+    .header-weili{
+        width: 43px;
+        height: 60px;
+        position: absolute;
+        background: url("../../public/images/wei.png") no-repeat;
+        background-size: 100% 100%;
+        top: -56px;
+        right: -23px;
     }
 </style>
